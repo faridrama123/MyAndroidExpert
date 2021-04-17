fbdf5b577a3563d3af1716983550c89bb8188894
faridrama123 <systemcalls915@gmail.com>
2021-04-17 13:10:26 +0700

    Revert "Coroutines Flow Sample"
    This reverts commit 91acafbe10ef70a8a50c9e454e09d7663fbcc411.

--- MyReactiveSearch/app/src/main/java/com/dicoding/myreactivesearch/model/PlaceResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.dicoding.myreactivesearch.model
-
-import com.google.gson.annotations.SerializedName
-
-data class PlaceResponse(
-	@field:SerializedName("features")
-	val features: List<PlacesItem>
-)
