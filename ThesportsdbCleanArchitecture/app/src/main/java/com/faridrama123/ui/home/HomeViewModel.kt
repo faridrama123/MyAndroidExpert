7e298851c08272c86a48f276e7063fe29a849840
faridrama123 <systemcalls915@gmail.com>
2021-05-16 19:56:05 +0700

    no message

--- ThesportsdbCleanArchitecture/app/src/main/java/com/faridrama123/ui/home/HomeViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.faridrama123.ui.home
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-
-}
---
+++ ThesportsdbCleanArchitecture/favorites/src/main/java/com/faridrama123/favorites/NotificationsFragment.kt
@@ -0,0 +1,60 @@
+package com.faridrama123.ui.notifications
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.faridrama123.databinding.FragmentNotificationsBinding
+import com.faridrama123.core.ui.TeamAdapter
+import com.faridrama123.ui.detail.DetailTeamActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NotificationsFragment : Fragment() {
+
+    private val notificationsViewModel : NotificationsViewModel by viewModels()
+    private lateinit var binding: FragmentNotificationsBinding
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNotificationsBinding.inflate(inflater,container,false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(activity !=null){
+
+            val favoritesTeamAdapter = TeamAdapter()
+            favoritesTeamAdapter.onItemClick = { selectedItem ->
+                val intent = Intent(activity, DetailTeamActivity::class.java)
+                intent.putExtra(DetailTeamActivity.EXTRA_DATA, selectedItem)
+                startActivity(intent)
+            }
+
+            notificationsViewModel.favoriteTeam.observe(viewLifecycleOwner, { dataTeam ->
+            favoritesTeamAdapter.setData(dataTeam)
+            })
+
+            with(binding.rvTeam) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = favoritesTeamAdapter
+            }
+
+        }
+
+    }
+
+
+
+}
